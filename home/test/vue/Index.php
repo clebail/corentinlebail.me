739ee5b1fcb637e593760c86d75a07df75e00b20
Corentin Lebail <corentin.lebail@gmail.com>
2019-06-19 16:50:16 +0200

    Menu for login

--- home/test/vue/Index.php
+++ home/test/vue/Index.php
@@ -1,11 +1,19 @@
 <?php
 class Home_Test_Vue_Index extends Home_Vue_Abstract {
+    public function __construct($params) {
+        parent::__construct($params);
+        
+        $this->addJs("/js/jquery-dropdown.js");
+    }
+    
     protected function createBreadCrumbs($datas) {
         $breadCrumbs[] = array("key" => "<i class='material-icons'>home</i>", "url" => Home_Controller_Index::getUrl());
         $breadCrumbs[] = array("key" => "Test", "url" => Home_Test_Controller_Index::getUrl());
         
         return $breadCrumbs;
-    }public function getTitle($datas) {
+    }
+    
+    public function getTitle($datas) {
         return "Test - Corentin Lebail";
     }
     
@@ -19,5 +27,9 @@ class Home_Test_Vue_Index extends Home_Vue_Abstract {
     
     public function getCurrentUrl() {
         return Home_Test_Controller_Index::getUrl("index", "index", array(), true);
+    }
+    
+    public function render($datas) {
+        echo $this->callTemplate("test/index", $datas);
     }
 }
